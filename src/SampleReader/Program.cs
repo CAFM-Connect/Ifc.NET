cf5dcd2728603aebf9d0d763f7517edb015d6310
thoffeller <th@cad-development.de>
2016-09-12 11:30:50 +0200

    extended SampleIfcReader

--- src/SampleReader/Program.cs
+++ src/SampleReader/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace SampleIfcReader
 {
     class Program
     {
+        [STAThreadAttribute()]
         static void Main(string[] args)
         {
-            // TODO : implementation of sample reader
+            Ifc.NET.Document ifcDocument;
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Ifc xml files (*.ifcxml)|*.ifcxml|All files (*.*)|*.*";
+            dlg.InitialDirectory = Path.GetDirectoryName(typeof(SampleIfcReader.Program).Assembly.Location);
+            dlg.CheckFileExists = true;
+            dlg.Multiselect = false;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                ifcDocument = Ifc.NET.Workspace.CurrentWorkspace.OpenDocument(dlg.FileName);
+            }
         }
     }
 }
