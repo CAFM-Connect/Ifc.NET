37882a51fab6549a6e247bf1c5405becd1286d69
klacol <klaus.aengenvoort@etask.de>
2016-09-25 09:22:48 +0200

    Fixes #2

--- src/Ifc.NET/Properties/AssemblyInfo.cs
+++ src/Ifc.NET/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ifc4")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ifc.NET")]
+[assembly: AssemblyDescription("Simple c# classes to read and write an File in Format IFC 4 XML")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ifc4")]
+[assembly: AssemblyCompany("Ifc.NET")]
+[assembly: AssemblyProduct("Ifc.NET")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
